6d5f93a66175455326420d1b3256dabd6b64d8c9
Your Name <you@example.com>
2019-05-14 18:12:28 +0530

    changes in json

--- barcode/barcode/doctype/barcode_print/barcode_print.js
+++ barcode/barcode/doctype/barcode_print/barcode_print.js
@@ -6,3 +6,7 @@ frappe.ui.form.on('Barcode Print', {
 
 	}
 });
+
+cur_frm.add_fetch("item_code","barcode","barcode")
+cur_frm.add_fetch("item_code","item_name","item_name")
+cur_frm.add_fetch("item_code","standard_rate","item_price")
